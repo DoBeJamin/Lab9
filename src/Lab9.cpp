fd952416843083eebb26b181217030ed8431a485
DoBeJamin <benjaminick11@gmail.com>
2023-07-04 17:48:36 -0500

    Added extra button to lock down

--- src/Lab9.cpp
+++ src/Lab9.cpp
@@ -13,6 +13,7 @@
 #include "SparkFun_VCNL4040_Arduino_Library.h"
 
 void setup();
+void release_lockdown();
 void loop();
 void format_display ();
 #line 9 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab9/src/Lab9.ino"
@@ -36,6 +37,7 @@ OledWingAdafruit display;
 //internal variables
 bool setup_mode = true;
 bool button_pressed = false;
+bool lock_down = false;
 
 int light_level_flags[2] = {0,0};
 int light_level_flag_index = 0;
@@ -49,6 +51,7 @@ double voltage;
 double temperature_celcius;
 double temperature_fahrenheit;
 
+Timer timer(10000, release_lockdown);
 
 void setup() {
 
@@ -83,75 +86,95 @@ void setup() {
 
 }
 
-void loop() {
+BLYNK_WRITE(V3) {
+  //displays lock down method and turns the lockdown variable on
+  lock_down = true;
+  format_display();
+  display.println("LOCK DOWN!");
+  display.display();
+  timer.start();
 
+}
+
+void release_lockdown() {
+  lock_down = false;
+}
+
+
+
+void loop() {
   //maintain display and blynk
   display.loop();
   Blynk.run();
 
-  //activates setup mode
-  if (display.pressedA()) {
-    setup_mode = true;
-  }
-  
-  //calculate temperatures
-  reading = analogRead(Temperature);
-  voltage = (reading * 3.3) / 4095.0;
-  temperature_celcius = (voltage - 0.5) * 100;
-  temperature_fahrenheit = (temperature_celcius * 9.0 / 5.0) + 32.0;
+  //stops everything from happening unless completely neccesary if lock down mode is on
+  if (!lock_down) {
 
-  //sends temps to blynk
-    Blynk.virtualWrite(V0, temperature_fahrenheit);
-    Blynk.virtualWrite(V1, temperature_celcius);
+    //activates setup mode
+    if (display.pressedA()) {
+      setup_mode = true;
+    }
+    
+    //calculate temperatures
+    reading = analogRead(Temperature);
+    voltage = (reading * 3.3) / 4095.0;
+    temperature_celcius = (voltage - 0.5) * 100;
+    temperature_fahrenheit = (temperature_celcius * 9.0 / 5.0) + 32.0;
 
-  // only changes the light_level_flag_index once when the button is pressed
-  if (digitalRead(Button))
-  {
-    button_pressed = true;
-  }
-  else if (button_pressed)
-  {
-    light_level_flag_index++;
-    if (light_level_flag_index == 2)
+    //sends temps to blynk
+      Blynk.virtualWrite(V0, temperature_fahrenheit);
+      Blynk.virtualWrite(V1, temperature_celcius);
+
+    // only changes the light_level_flag_index once when the button is pressed
+    if (digitalRead(Button))
     {
-      setup_mode = false;
-      light_level_flag_index = 0;
+      button_pressed = true;
     }
-    button_pressed = false;
-  }
-
-  if (setup_mode) {
-    //displays information for setting values and allows the user to set a value
-    light_level_flags[light_level_flag_index] = analogRead(Potentiometer);
-
-    format_display();
-    display.print("Choose limit ");
-    display.println(light_level_flag_index+1);
-    display.println("range: 0,4095 ");
-    display.print(light_level_flags[0]);
-    display.print(", ");
-    display.println(light_level_flags[1]);
-
-    display.display();
-
-  } else {
-
-    //format screen and text
-    format_display();
-    display.println("TEMPERATURE");
-    display.print(temperature_celcius);
-    display.print("C : ");
-    display.print(temperature_fahrenheit);
-    display.print("F");
-
-    //display screen
-    display.display();
-
-      //sends alerts
-    if (current_range != old_range) {
-      //Blynk.logEvent("rangechange", current_range); 
+    else if (button_pressed)
+    {
+      light_level_flag_index++;
+      if (light_level_flag_index == 2)
+      {
+        setup_mode = false;
+        light_level_flag_index = 0;
+      }
+      button_pressed = false;
     }
-    old_range = current_range;
+
+    if (setup_mode) {
+      //displays information for setting values and allows the user to set a value
+      light_level_flags[light_level_flag_index] = analogRead(Potentiometer);
+
+      format_display();
+      display.print("Choose limit ");
+      display.println(light_level_flag_index+1);
+      display.println("range: 0,4095 ");
+      display.print(light_level_flags[0]);
+      display.print(", ");
+      display.println(light_level_flags[1]);
+
+      display.display();
+
+    } else {
+
+      //format screen and text
+      format_display();
+      display.println("TEMPERATURE");
+      display.print(temperature_celcius);
+      display.print("C : ");
+      display.print(temperature_fahrenheit);
+      display.print("F");
+
+      //display screen
+      display.display();
+
+        //sends alerts
+      if (current_range != old_range) {
+        Blynk.logEvent("rangechange", current_range); 
+      }
+      old_range = current_range;
+
+    }
   }
 
 
@@ -172,6 +195,8 @@ void loop() {
     digitalWrite(LED3, HIGH);
     current_range = 3;
   }
+
+
 
 }
 
